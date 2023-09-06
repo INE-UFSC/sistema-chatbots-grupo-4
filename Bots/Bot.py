c043567bd37ddfa7fb15f68f4adb18352588d6b0
Victória Rodrigues Veloso <victoria.rvv@outlook.com>
2023-09-06 14:12:12 -0300

    Bot fin

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -9,13 +9,12 @@ class Bot(ABC):
         self.__nome = nome
         self.__comandos = {}
 
-    #nao esquecer o decorator
 
     @property
     def nome(self):
         return self.__nome
 
-    #nao esquecer o decorator
+
     @nome.setter
     def nome(self,nome):
         self.__nome = nome
@@ -25,16 +24,22 @@ class Bot(ABC):
         return self.__comandos
 
     @comandos.setter
-    def comandos(self,comandos):
-        self.comandos = self.__comandos
+    def comandos(self, comandos):
+        self.__comandos = comandos
+
 
     def mostra_comandos(self):
-        pass
-
+        self.comandos[1] = "Quero uma opinião sobre o meu treino"
+        self.comandos[2] = "Treino para braço "
+        self.comandos[3] = "Quero uma opinião sobre o meu shape"
+        self.comandos[4] = "Adeus"
+        return self.comandos
+    
     @abstractmethod
     def executa_comando(self,cmd):
         pass
 
+        
     @abstractmethod
     def boas_vindas(self):
         pass
@@ -42,3 +47,50 @@ class Bot(ABC):
     @abstractmethod
     def despedida(self):
         pass
+
+class MeuBot(Bot):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+
+    def executa_comando(self, cmd):
+        if cmd == '1':
+            return "\n=================\nApós uma avaliação minuciosa, percebi que você é um FRANGO !\n=================\n"
+        if cmd =='2':
+            return f"\n=================\n- Comece alongando os braços\n- 3 séries de 2 minutos tentando cortar o bife do RU\n- 2 séries de 2 minutos tentando cortar a bisteca do RU\n=================\n"
+        if cmd =='3':
+            return "\n=================\nLook at him. Nem parece que treina... Braços finos, corpo compacto e pouco aesthetic\n=================\n"
+        if cmd =='4':
+            return "\n=================\nTudo de melhor pra você ! OUT\n=================\n"
+    
+    def boas_vindas(self):
+        
+        print("Qual mensagem vem aqui nas boas vindas ?")
+    
+    def despedida(self):
+        print("Volte sempre meu querido Fake Natty !")
+
+
+# Criando uma instância do bot
+meu_bot = MeuBot("BotFit")
+
+##PARA TESTE DE FUNCIONAMENTO
+
+while True:
+
+
+
+    comandos = meu_bot.mostra_comandos()
+    for chave, valor in comandos.items():
+        print(f"{chave} - {valor}")
+
+    #exibe os comandos
+    controle = input("\nDigite o comando desejado (ou -1 para fechar o programa): ")
+    # Configurando comandos
+
+    if controle == '-1':
+        meu_bot.despedida()
+        break
+    else:
+        executa =  meu_bot.executa_comando(controle)
+        print(executa)
